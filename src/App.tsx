1a335338fafee5733d06f9fba9623fa360974d89
nibchee <nirbhaybansal28@gmail.com>
2023-06-26 15:42:46 +0530

    React Router SetUp

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <SearchBooksPage />
-      <HomePage />
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/hoeme'>
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
