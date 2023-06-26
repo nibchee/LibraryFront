812b019aad4e792a18f493132b7394be3c3fbd90
nibchee <nirbhaybansal28@gmail.com>
2023-06-26 16:37:11 +0530

    UseEffect on statechnage added

--- src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
+++ src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
@@ -1,15 +1,64 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckOutPage = () => {
 
     //creating states
     const [book, setBook] = useState<BookModel>();
-    const [isLoadingBook, setIsLoadingBook] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
     //this stores the book id of which we need information eg //locahost/bookcheckout/1
     const bookId = (window.location.pathname).split('/')[2];
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const response = await fetch(baseUrl);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const loadedBook: BookModel = {
+                id: responseJson.id,
+                title: responseJson.title,
+                author: responseJson.author,
+                description: responseJson.description,
+                copies: responseJson.copies,
+                copiesAvailable: responseJson.copiesAvailable,
+                category: responseJson.category,
+                img: responseJson.img,
+            };
+
+            setBook(loadedBook);
+            setIsLoading(false);
+        };
+        fetchBook().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    //if API taking time
+    if (isLoading) {
+        return (
+            <SpinnerLoading />
+        )
+    }
+    //if there is http Error
+    if (httpError) {
+        return (
+            <div className="conatiner m-5">
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h3>
