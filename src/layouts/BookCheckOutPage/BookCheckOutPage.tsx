978901760b58f9a72a39c1b1094280ec8b0ae32a
nibchee <nirbhaybansal28@gmail.com>
2023-07-02 05:23:21 +0530

    All Reviews Page prepared

--- src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
+++ src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarReview } from "../Utils/StarReviews";
