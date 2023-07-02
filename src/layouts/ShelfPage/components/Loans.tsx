8c3d66707fa11833aca861c9a6d8443db5202d66
nibchee <nirbhaybansal28@gmail.com>
2023-07-02 17:12:00 +0530

    Loans Page Componnet added in Shelf Page

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -2,6 +2,7 @@ import { useOktaAuth } from "@okta/okta-react"
 import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 
 export const Loans = () => {
@@ -15,7 +16,7 @@ export const Loans = () => {
     useEffect(() => {
         const fetchUserCurrentLoans = async () => {
             if (authState && authState.isAuthenticated) {
-                const url = `http://localhost:8080/apibooks/secure/currentloans`;
+                const url = `http://localhost:8080/api/books/secure/currentloans`;
                 const requestOptions = {
                     method: 'GET',
                     headers: {
@@ -61,6 +62,79 @@ export const Loans = () => {
         );
     }
 
+    return (
+        <div>
+            {/*Desktop*/}
+            <div className="d-none d-lg-block mt-2">
+                {shelfCurrentLoans.length > 0 ?
+                    <>
+                        <h5>Current Loans:</h5>
+                        {shelfCurrentLoans.map(shelfCurrentLoans => (
+                            <div key={shelfCurrentLoans.book.id}>
+                                <div className="row mt-3 mb-3">
+                                    <div className="col-4 col-md-4 container">
+                                        {shelfCurrentLoans.book?.img ?
+                                            <img src={shelfCurrentLoans.book?.img} width='226' height='349' alt='Book' />
+                                            :
+                                            <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                                                width='226' height='349' alt="Book" />
+                                        }
+                                    </div>
+                                    <div className="card col-3 col-md-3 container d-flex">
+                                        <div className="card-body">
+                                            <div className="mt-3">
+                                                <h4>Loan Options</h4>
+                                                {shelfCurrentLoans.daysLeft > 0 &&
+                                                    <p className="text-secondary">
+                                                        Due in {shelfCurrentLoans.daysLeft} days.
+                                                    </p>
+                                                }
+                                                {shelfCurrentLoans.daysLeft == 0 &&
+                                                    <p className="text-success">
+                                                        Due Today.</p>
+                                                }
+                                                {shelfCurrentLoans.daysLeft < 0 &&
+                                                    <p className="text-danger">
+                                                        Past due by {shelfCurrentLoans.daysLeft} days.
+                                                    </p>
+                                                }
+                                                <div className="list-group mt-3">
+                                                    <button className="list-group-item list-group-item-action"
+                                                        aria-current='true' data-bs-toggle='modal'
+                                                        data-bs-target={`#modal${shelfCurrentLoans.book.id}`}>
+                                                        Manage Loan
+                                                    </button>
+                                                    <Link to={'search'} className="list-group-item list-group-item-action">
+                                                        Search more books?
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <p className="mt-3">
+                                                Help other find thier adventure by reviewing your loan.
+                                            </p>
+                                            <Link className="btn btn-primary" to={`/checkout/${shelfCurrentLoans.book.id}`}>
+                                                Leave a review.
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                        ))}
+                    </> :
+                    <>
+                        <h3 className="mt-3">
+                            Currently no loans
+                        </h3>
+                        <Link className="btn btn-primary" to={`search`}>
+                            Search for a new book
+                        </Link>
+                    </>}
+            </div>
+            {/*Mobile*/}
 
+        </div>
+    )
 
 }
