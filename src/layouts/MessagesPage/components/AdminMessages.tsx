c9c83cfd9e1b9015f59616328a8142cf27d923ea
nibchee <nirbhaybansal28@gmail.com>
2023-07-07 22:51:49 +0530

    AddBookRequest Model

--- src/layouts/MessagesPage/components/AdminMessages.tsx
+++ src/layouts/MessagesPage/components/AdminMessages.tsx
@@ -1,9 +1,10 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import MessageModel from "../../../models/MessageModel";
-import { error } from "console";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Pagination } from "../../Utils/Pagination";
+import { AdminMessage } from "./AdminMessage";
+import AdminMessageRequest from "../../../models/AdminMessageRequest";
 
 export const AdminMessages = () => {
 
@@ -20,6 +21,9 @@ export const AdminMessages = () => {
     // Pagination
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
+
+    // Recall useEffect
+    const [btnSubmit, setBtnSubmit] = useState(false);
 
     useEffect(() => {
         const fetchUserMessages = async () => {
@@ -49,7 +53,7 @@ export const AdminMessages = () => {
             setHttpError(error.message);
         })
         window.scrollTo(0, 0);
-    }, [authState, currentPage]);
+    }, [authState, currentPage, btnSubmit]);
 
     if (isLoadingMessages) {
         return (
@@ -65,21 +69,41 @@ export const AdminMessages = () => {
         );
     }
 
+    async function submitResponseToQuestion(id: number, response: string) {
+        const url = `http://localhost:8080/api/messages/secure/admin/message`;
+        if (authState && authState?.isAuthenticated && id !== null && response !== '') {
+            const messageAdminRequestModel: AdminMessageRequest = new AdminMessageRequest(id, response);
+            const requestOption = {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(messageAdminRequestModel)
+            }
+            const messageAdminRequestModelResponse = await fetch(url, requestOption);
+            if (!messageAdminRequestModelResponse.ok) {
+                throw new Error('Something went wrong!');
+            }
+            setBtnSubmit(!btnSubmit);
+        }
+    }
+
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
     return (
         <div className="mt-3">
-            {messages.length>0?
-            <>
-            <h5>Pending Q/A:</h5>
-            {messages.map(messages=>(
-                <p>Questions that need a response</p>
-            ))}
-            </>
-            :
-            <h5>No Pending Q/A</h5>
+            {messages.length > 0 ?
+                <>
+                    <h5>Pending Q/A:</h5>
+                    {messages.map(message => (
+                        <AdminMessage message={message} key={message.id} submitResponseToQuestion={submitResponseToQuestion} />
+                    ))}
+                </>
+                :
+                <h5>No Pending Q/A</h5>
             }
-            {totalPages>1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
+            {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />}
         </div>
     );
 }
